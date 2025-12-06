804fd249f8f0f99a1d22912820cc1fb27de5c685
Matthew Teferi <metthewsteferi@gmail.com>
2025-12-05 22:36:21 -0800

    Add class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,14 +1,11 @@
 class Calculator:
-    
-    # Class attribute
     calculation_type = "Arithmetic Operations"
-    
+
     @staticmethod
-    def add(a: float, b: float) -> float:
+    def add(a, b):
         return a + b
-    
+
     @classmethod
-    def multiply(cls, a: float, b: float) -> float:
-        # Access class attribute
+    def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
